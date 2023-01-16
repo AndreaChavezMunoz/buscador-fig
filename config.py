468a2ed5652af70b640b7db6e72d8686b03155e5
Andrea Chavez Munoz <chavezmunoz.a@northeastern.edu>
2023-01-16 16:58:37 -0500

    Config user agent split

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ accepted_domains=['plazavea.com.pe','sodimac.com.pe',
 'listado.mercadolibre.com.pe','cahema.pe']
 
 # Change headers so not recognized as bot
-headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'}
+user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+headers = {'User-Agent':user_agent}
 
 # Filltro
 def agg(g):
