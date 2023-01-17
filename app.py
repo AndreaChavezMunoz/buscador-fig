c4c1726abf607b4522552410673870d3a410fb0b
Andrea Chavez Munoz <chavezmunoz.a@northeastern.edu>
2023-01-17 16:38:05 -0500

    debugging

--- app.py
+++ app.py
@@ -159,12 +159,17 @@ def buscar_precios(productosToSearch):
     pandas.DataFrame
         Products found online with domain, name, price, brand, link, and quantity.
     """
+    st.write(len(productosToSearch))
+
     if len(productosToSearch)==0:
         return pd.DataFrame()
 
     # Web scrape each product
     info_all = []
+    st.write('Search starting')
+    st.write(productosToSearch.index)
     for i in stqdm(productosToSearch.index,desc="Buscando los mejores precios"):
+        st.write('In loop')
         p=productosToSearch.at[i, 'Producto']
         n=productosToSearch.at[i,'Cantidad']
         info_found = searchEngine(p,n)
