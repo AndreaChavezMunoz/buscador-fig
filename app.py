368d414327547b4b2161f773b267176d6a2b9726
Andrea Chavez Munoz <chavezmunoz.a@northeastern.edu>
2023-01-17 16:41:33 -0500

    debugging

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ def buscar_precios(productosToSearch):
     st.write('Search starting')
     st.write(productosToSearch.index)
     st.write(range(3))
-    for i in stqdm(productosToSearch.index,desc="Buscando los mejores precios"):
+    for i in stqdm(range(len(productosToSearch.index)),desc="Buscando los mejores precios"):
         st.write('In loop')
         p=productosToSearch.at[i, 'Producto']
         n=productosToSearch.at[i,'Cantidad']
